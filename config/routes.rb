5c1c84e0fbd54c2dd842c5ce62a94dcad60a7633
Aurélien Hervé <aurelien@adomik.com>
2014-03-10 17:42:37 +0100

    connected controllers w/ bootstrap navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 ALEM::Application.routes.draw do
 
+  get "news/index",  :as => "news"
+  get "news/show"
   root :to => "home#index"
-
-  get "photos/index"
-  get "videos/index"
-  get "music/index"
   get "home/index"
+
+  get "photos/index", :as => "photos"
+  get "videos/index", :as => "videos"
+  get "music/index" , :as => "music"
+
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
 end
