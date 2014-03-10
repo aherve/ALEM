90bf3bb87ef30827a13946e13691ec1f7e3f9611
aurelien herve <mail@aurelien-herve.com>
2014-03-10 20:45:03 +0100

    news

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -1,2 +1,11 @@
 module NewsHelper
+
+  def short_desc(actu)
+    s = "<h3>"
+    s << ((d = I18n.l(actu.date_time)).empty? ? "" : d+" - " )
+    s << actu.title
+    s << "</h3>"
+    raw s
+  end
+
 end
--- app/models/news.rb
+++ app/models/news.rb
@@ -1,8 +1,9 @@
 class News
   include Mongoid::Document
   field :title, type: String
+  field :short_description, type: String
   field :content, type: String
-  field :date_time, type: DateTime
+  field :date_time, type: Date
 
   validates_presence_of :title, :content
 end
