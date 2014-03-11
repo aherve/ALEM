f3b296eae832d8f7402aa14a0108de3b3c1591e4
Aurélien Hervé <aurelien@adomik.com>
2014-03-11 14:23:00 +0100

    news validation

--- app/models/news.rb
+++ app/models/news.rb
@@ -7,9 +7,10 @@ class News
   field :content, type: String
   field :date_time, type: Date
 
-  validates_presence_of :title, :content
-  
+  validates_presence_of :title, :content, :date
   validates_format_of :url, :with => /\A[a-zA-z0-9%-_]+\z/
+
+  validates_uniqueness_of :url
 
   def to_param
     url
