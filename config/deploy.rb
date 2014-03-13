89f93e1bf0e3bab7bd367ca6a04db7e4908ba557
aurelien herve <mail@aurelien-herve.com>
2014-03-13 12:48:11 +0100

    asset_path

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,10 +32,10 @@ set :deploy_to, '/var/www/alem'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/mongoid.yml config/initializers.secret_token.rb}
 
 # Default value for linked_dirs is []
- set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads config/mongoid.yml config/boot.rb}
+ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
