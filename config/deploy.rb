c05684a184d31b0d972d6e8208ff1788417ee7e7
Aurélien Hervé <aurelien@adomik.com>
2014-03-24 15:37:44 +0100

    sitemap task added to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,10 +52,6 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-
-       #refresh sitemap: 
-       execute :rake, 'sitemap:refresh'
-
       # Your restart mechanism here, for example:
        execute :touch, release_path.join('tmp/restart.txt')
 
@@ -67,9 +63,10 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+        #   execute :rake, 'cache:clear'
+        execute :rake, 'sitemap:refresh'
+      end
     end
   end
 
