b76164010140e45a7ba92e41f724cc36b1d03c34
aurelien herve <mail@aurelien-herve.com>
2014-05-31 14:19:16 +0200

    lol gte

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @latest_news = News.gte(date: Date.today - 1).sort_by(&:date) 
+    @latest_news = News.gt(date: Date.today - 1).sort_by(&:date) 
     @photos = Photo.where(:published => true)
     @videos = Video.all.sort_by(&:created_at).reverse
   end
